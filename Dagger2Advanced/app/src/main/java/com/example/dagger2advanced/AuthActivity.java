e03706037ff84cfd168687ad2465621a842bf564
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-02 18:52:12 +0300

    referencing multiple dagger dependencies with a single module

--- Dagger2Advanced/app/src/main/java/com/example/dagger2advanced/AuthActivity.java
+++ Dagger2Advanced/app/src/main/java/com/example/dagger2advanced/AuthActivity.java
@@ -1,9 +1,10 @@
 package com.example.dagger2advanced;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
 
 import javax.inject.Inject;
 
@@ -14,17 +15,22 @@ public class AuthActivity extends DaggerAppCompatActivity {
     private static final String TAG = "AuthActivity";
 
     @Inject
-    String someString;
+    Drawable logo;
 
     @Inject
-    boolean isAppNull;
+    RequestManager requestManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        Log.d(TAG,"onCreate : " + someString);
-        Log.d(TAG,"onCreate : is app null "+ isAppNull);
+        setLogo();
     }
+
+    private void setLogo() {
+        requestManager.load(logo)
+                .into((ImageView) findViewById(R.id.login_logo));
+    }
+
 }
--- Dagger2Advanced/app/src/main/java/com/example/dagger2advanced/di/AppModule.java
+++ Dagger2Advanced/app/src/main/java/com/example/dagger2advanced/di/AppModule.java
@@ -1,6 +1,14 @@
 package com.example.dagger2advanced.di;
 
 import android.app.Application;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.dagger2advanced.R;
 
 import dagger.Module;
 import dagger.Provides;
@@ -9,13 +17,22 @@ import dagger.Provides;
 public class AppModule {
 
     @Provides
-    static String someString(){
-        return "test string";
+    static RequestOptions provideRequestOptions(){
+        return RequestOptions.placeholderOf(R.drawable.white_background)
+                .error(R.drawable.white_background);
     }
 
     @Provides
-    static boolean getApp(Application application){
-        return application == null;
+    static RequestManager provideGlideInstance (Application application
+            ,RequestOptions requestOptions){
+        return Glide.with(application)
+                .setDefaultRequestOptions(requestOptions);
     }
+
+    @Provides
+    static Drawable provideAppDrawable(Application application){
+        return ContextCompat.getDrawable(application,R.drawable.logo);
+    }
+
 
 }

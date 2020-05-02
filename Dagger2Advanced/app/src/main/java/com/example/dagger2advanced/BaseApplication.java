66cf845c878fd1c4232298a6cd5274039d79fc06
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-02 16:39:35 +0300

    application component

---
+++ Dagger2Advanced/app/src/main/java/com/example/dagger2advanced/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.example.dagger2advanced;
+
+
+import com.example.dagger2advanced.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
---
+++ Dagger2Advanced/app/src/main/java/com/example/dagger2advanced/di/AppComponent.java
@@ -0,0 +1,28 @@
+package com.example.dagger2advanced.di;
+
+import android.app.Application;
+
+import com.example.dagger2advanced.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+
+}
